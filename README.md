e3b984f1829f968dbfb93f9d707a5507cacb028c
rodx1977 <rodx1977@gmail.com>
2022-07-29 22:45:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-dvkm3a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-dvkm3a)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Rodolfo Rodriguez</h1>
+<h2>Test</h2>`;
